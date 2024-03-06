37daaf5f36950ad54c5e4cf1e561b36bb64811fe
victorivodev <victorivo.comercial@gmail.com>
2024-03-06 20:24:21 -0300

    Primeira versão funcional da aplicação

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+app.use(express.static(__dirname + '/public'));
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser")
 const Contato = require("./models/Contato");
-const { sequelize } = require("./models/database");
+const { sequelize } = require("./database/database");
 const Swal = require('sweetalert2')
 const porta = 8080;
 
@@ -13,12 +14,13 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
 //Rotas
 app.get('/', function(req, res) {
     Contato.findAll().then(function(contatos){
-        res.render('home', {contatos: contatos})
-    })
-})
+        res.render('home', {contatos: contatos});
+    });
+});
 
 app.get('/addcontato', function(req, res) {
     res.render('addContato')
@@ -35,11 +37,25 @@ app.post('/cadContato', function(req, res){
         email: req.body.email,
         telefone: req.body.telefone
     }).then(function(){
-        res.redirect('/');
+        res.json({ success: true });
     }).catch(function(err){
-        res.send("Não foi possível cadastrar o contato. Erro: " + err)
+        res.json({ error: "Não foi possível cadastrar o contato. Erro: " + err });
     });
 });
+
+app.post('/createContato', function(req, res){
+    Contato.create({
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        email: req.body.email,
+        telefone: req.body.telefone
+    }).then(function(){
+        res.json({ success: true });
+    }).catch(function(err){
+        res.json({ error: "Não foi possível cadastrar o contato. Erro: " + err });
+    });
+});
+
 
 
 app.get('/delContato/:id', function(req, res){
@@ -53,6 +69,29 @@ app.get('/delContato/:id', function(req, res){
     res.send("Erro ao excluir contato. Erro: " + err)
   });
 })
+
+app.post('/editContato/:id', function(req, res){
+    Contato.update({
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        email: req.body.email,
+        telefone: req.body.telefone
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(function(result){
+        if (result[0] === 0) {
+            res.json({ error: "Contato não encontrado." });
+        } else {
+            res.json({ success: true });
+        }
+    }).catch(function(err){
+        res.json({ error: "Erro ao editar contato. Erro: " + err });
+    });
+});
+
+
 
 app.listen(porta, function(err) {
     if(err) {
--- models/Contato.js
+++ models/Contato.js
@@ -1,4 +1,4 @@
-const database = require('./database');
+const database = require('../database/database');
 
 const Contato = database.sequelize.define('contatos', {
     id: {
